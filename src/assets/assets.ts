9c0c5a3a10b1938436ef157b018431c9e83cba7f
tmtuan04 <tuminhtuana7@gmail.com>
2024-10-14 15:05:43 +0700

    update login/signup v1 by tuan

---
+++ src/assets/assets.ts
@@ -0,0 +1,9 @@
+import email from "./email.png"
+import person from "./person.png"
+import password from "./password.png"
+
+export const assets = {
+    email,
+    person,
+    password
+}
--- src/pages/login-signup/LoginSignUp.tsx
+++ src/pages/login-signup/LoginSignUp.tsx
@@ -1,24 +1,74 @@
-// Import các file và hình ảnh cần thiết => Nên tạo file js để lưu ảnh thì đỡ phải import nhiều
 import "../../styles/LoginSignUp.css";
-// import user_icon from "../../assets/person.png";
-// import email_icon from "../../assets/email.png";
-// import password_icon from "../../assets/password.png";
+import { assets } from "../../assets/assets";
 // import { useState } from "react";
-
+import Button from "../../components/Button";
 
 // Component LoginSignUp dùng để hiển thị giao diện đăng nhập và đăng ký
 export const LoginSignUp = () => {
-
   return (
     <div className="container" id="container">
-      <div className="form-container signup"></div>
+      <div className="form-container signup">
         <form action="">
-          <h1>Create Account</h1>
-          <input type="text" placeholder="UserName"/>
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
-          <button>Sign Up</button>
+          {/* Đoạn này cần check lại xem có cần tạo hai cái không */}
+          <div className="header">
+            <h1>Create Account</h1>
+            {/* Gạch ngang dưới chân cho đẹp */}
+            <div className="underline"></div>
+          </div>
+          <div className="input">
+            <img src= {assets.person} alt="" />
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="input">
+            <img src= {assets.email} alt="" />
+            <input type="email" placeholder="Email" />
+          </div>
+          <div className="input">
+            <img src= {assets.password} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="input">
+            <img src= {assets.password} alt="" />
+            <input type="password" placeholder="Confirm password" />
+          </div>
+          <Button text="Sign Up" type="submit"></Button>
+          
         </form>
+      </div>
+      <div className="form-container signin">
+        <form action="">
+          <div className="header">
+            <h1>Sign In</h1>
+            <div className="underline"></div>
+          </div>
+          <div className="input">
+            <img src= {assets.person} alt="" />
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="input">
+            <img src= {assets.password} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+          <a href="#">Forgot Your Password?</a>
+          <Button text="Sign In" type="submit"></Button>
+        </form>
+      </div>
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel-toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of site features</p>
+            {/* <button className="hidden" id="login">Sign In</button> */}
+            <Button text="Sign In" className="hidden" id="login"></Button>
+          </div>
+          <div className="toggle-panel-toggle-right">
+            <h1>Hello Friend!</h1>
+            <p>Register with your personal details to use all of site features</p>
+            {/* <button className="hidden" id="register">Sign Up</button> */}
+            <Button text="Sign Up" className="hidden" id="register"></Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
