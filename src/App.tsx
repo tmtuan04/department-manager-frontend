05ac9edfe0769d19bb7db9261fce3010720884db
Tran Trong Nguyen <kenzoropp@gmail.com>
2024-11-08 21:27:19 +0700

    Thêm css khi hover toggle menu sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import HomePage from './pages/home-page/HomePage'
+import { LoginSignUp } from './pages/login-signup/LoginSignUp'
+import DashBoard from './pages/dashboard/DashBoard'
 
 const App = () => {
   return (
-    <HomePage />
+    <DashBoard />
   )
 }
 
